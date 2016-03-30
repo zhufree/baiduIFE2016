64abb3e7020fbd3e45eff943c516952adc89c174
zhufree <zhufree2013@gmail.com>
2016-03-30 10:41:06 +0800

    add task19

--- project-2/task19/task19.js
+++ project-2/task19/task19.js
@@ -5,6 +5,18 @@ var inRight = document.getElementById('inRight');
 var outLeft = document.getElementById('outLeft');
 var outRight = document.getElementById('outRight');
 var inp = document.getElementsByTagName('input')[0];
+var sortBtn = document.getElementById('sort');
+
+function render() {
+    var boxLen = showBox.childNodes.length;
+    for (var i = 0; i < boxLen; i++) {
+        curBox = showBox.firstChild;
+        showBox.removeChild(curBox);
+    };
+    for (var i = 0; i < arr.length; i++) {
+        showBox.appendChild(createBox(arr[i]));
+    };
+};
 
 function removeBox(e) {
     for (var i = 0; i < showBox.childNodes.length; i++) {
@@ -19,6 +31,7 @@ function createBox(num) {
     var newBox = document.createElement('div');
     newBox.setAttribute('class', 'box');
     newBox.innerText = num;
+    newBox.style.height = num * 2 + 'px';
     newBox.onclick = removeBox;
     return newBox;
 };
@@ -56,12 +69,27 @@ function removeFromRight() {
     showBox.removeChild(lastBox);
 }
 
+function sort() {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        };
+    };
+    render();
+}
+
 function checkNum() {
     var num = inp.value;
-    if (isNaN(num.trim() - 0)) {
-        alert('Please Enter A Number!');
+    if (isNaN(num.trim() - 0) || num < 10 || num > 100) {
+        alert('Please Enter A Number in 10-100!');
         inp.value = '';
         return false;
+    } else if (arr.length > 60) {
+        alert('Array is Full!');
     } else {
         return num;
     }
@@ -71,4 +99,4 @@ inLeft.onclick = enterFromLeft;
 inRight.onclick = enterFromRight;
 outLeft.onclick = removeFromLeft;
 outRight.onclick = removeFromRight;
-
+sortBtn.onclick = sort;
