876ead6cc3a572e43b760b6b94d4e4804241cb45
zhufree <zhufree2013@gmail.com>
2016-08-04 10:54:57 +0800

    'add30'

---
+++ project-2/task28/task28.js
@@ -0,0 +1,266 @@
+// 未完成
+$(function() {
+    var SHIP_POWER_TYPES = [{
+            velocity: 30,
+            reduce: 5
+        }, {
+            velocity: 50,
+            reduce: 7
+        }, {
+            velocity: 80,
+            reduce: 9
+        }];//can change to a function to create
+    var SHIP_ENERGY_TYPES = [2, 3, 4];
+    var SHIP_POWER_FULL = 100;
+    var SHIP_MAX = 4;
+    var shipSlot = [null, null, null , null];
+    var shipCount = 0;
+
+    // 创建飞船的DOM
+    function createShip(ship) {
+        var shipEnergy = $('<div>', {'class': 'energy'});
+        var shipSelf = $('<div>', {
+            'class': 'ship',
+            'id': ship.orbit,
+        });
+        var shipPole = $('<div>', {'class': 'ship-pole'});
+        shipPole.height((ship.orbit + 1) * 100 + 'px');
+        shipSelf.append(shipEnergy);
+        shipPole.append(shipSelf);
+        return shipPole;
+    }
+    // 创建飞船对象
+    function Ship(orbit, powerTypeId, energyTypeId) {
+        if (shipCount < 4) {
+            if (!shipSlot[orbit - 1]) {
+                this.orbit = orbit; // 设置轨道槽
+                this.curPower = SHIP_POWER_FULL;
+                this.powerType = SHIP_POWER_TYPES[powerTypeId];
+                this.energyType = SHIP_ENERGY_TYPES[energyTypeId];
+                this.runStatus = false;
+                this.position = (100 - this.curPower)*3.6; //0deg
+                shipSlot[orbit - 1] = this;
+                this.shipDom = createShip(this);
+                this.shipDom.ship = this;
+                $('#display').append(this.shipDom);
+                console.log('Create a ship, id is ' + this.orbit);
+            }
+            else {
+                console.log('This orbit had a ship already!');
+            }
+        } else {
+            console.log('Cant launch more than 4 ships!');
+        }
+    }
+
+    Ship.prototype = {
+        launch: function() {
+          // 启动方法
+            var self = this;
+            if (!self.runStatus) {
+                self.runStatus = true;
+                console.log('Ship ' + self.orbit + ' launched!');
+                self.runInt = setInterval(function(){
+                    self.shipDom.css({'transform': 'rotate(' + self.position + 'deg)'});
+                    $('.energy', self.shipDom).width(self.curPower/SHIP_POWER_FULL % 100*50 + 'px');
+                    self.curPower -= self.powerType.reduce/10;
+                    self.position += 360/self.powerType.velocity/10;
+                    if ( self.curPower <= 0) {
+                        self.stop();
+                    }
+                }, 100);
+            } else {
+                console.log('Ship ' + self.orbit + ' is running!');
+            }
+        },
+        stop: function() {
+          // 停止方法
+            var self = this;
+            if (self.runStatus) {
+                self.runStatus = false;
+                if (!!self.runInt) {
+                    clearInterval(self.runInt);
+                }
+                console.log('Ship ' + self.orbit + ' stoped!');
+            } else {
+                console.log('Ship ' + self.orbit + ' is not running!');
+            }
+        },
+        recharge: function() {
+          // 充能
+            var self = this;
+            setInterval(function(){
+                $('.energy', self.shipDom).width(self.curPower/SHIP_POWER_FULL % 100*50 + 'px');
+                if (self.curPower < SHIP_POWER_FULL){
+                    self.curPower += self.energyType/10;
+                }
+            }, 100);
+        },
+        destroy: function() {
+          // 摧毁
+            // this = null;
+            console.log('destroying');
+            this.shipDom.remove();
+            shipSlot[this.orbit - 1] = null;
+            console.log('Ship ' + this.orbit + ' destroyed!');
+        },
+        /*
+        {
+            id: 1,
+            command: 'create'/stop'/'launch'/'destroy'
+        }
+        */
+        Adapter: function(signal) {
+          // 改成16位信号，并可以双向处理翻译信号
+          // 发送系统的翻译器
+          console.log(typeof signal);
+          if (typeof signal === 'object') {
+            var idLen = parseInt(signal['id']).toString(2).length;
+            var idBi = '0000'.substr(idLen).concat(parseInt(signal['id']).toString(2));
+            console.log(idBi);
+            var comBi;
+            switch (signal['command']){
+                case 'create':
+                    comBi = '0000';
+                    break;
+                case 'stop':
+                    comBi = '0010';
+                    break;
+                case 'launch':
+                    comBi = '0001';
+                    break;
+                case 'destroy':
+                    comBi = '1100'
+                    break;
+                default:
+                    console.log('Wrong command!');
+                    comBi = '1111';
+                    break;
+            }
+            return idBi + comBi;
+          } else {
+            var shipId = parseInt(signal.substr(0, 4), 2);
+            var shipStatusBi = parseInt(signal.substr(4, 8));
+            switch (shipStatusBi){
+                case '0000':
+                    shipStatus = 'create';
+                    break;
+                case '0010':
+                    shipStatus = 'stop';
+                    break;
+                case '0001':
+                    shipStatus = 'launch';
+                    break;
+                case '1100':
+                    shipStatus = 'destroy'
+                    break;
+                default:
+                    console.log('Wrong command!');
+                    shipStatus = 'Wrong command';
+                    break;
+            }
+            return {
+              'id': shipId,
+              'status': shipStatus,
+            }
+          }
+        }
+        signalRece: function(signalBi) {
+            var self = this;
+            // 接收系统的翻译器
+            function Adapter(signalBi) {
+                var shipId = parseInt(signalBi.substr(0, 4), 2);
+                switch (signalBi.substr(4, 7)){
+                    case '0000':
+                        comStr = 'create';
+                        break;
+                    case '0010':
+                        comStr = 'stop';
+                        break;
+                    case '0001':
+                        comStr = 'launch';
+                        break;
+                    case '1100':
+                        comStr = 'destroy'
+                        break;
+                    default:
+                        console.log('Wrong command!');
+                        break;
+                }
+                return {
+                    'id': shipId,
+                    'command': comStr
+                };
+            }
+            signal = Adapter(signalBi);
+            if (signal['id'] === self.orbit) {
+                switch (signal['command']){
+                    case 'stop':
+                        self.stop();
+                        break;
+                    case 'launch':
+                        self.launch();
+                        break;
+                    case 'destroy':
+                        self.destroy();
+                        break;
+                    default:
+                        console.log('wrong command!');
+                        break;
+                }
+            }
+        }
+    }
+
+    function BUS(signal) {
+
+        signalBi = Adapter(signal);
+        function FeedBack() {
+          setTimeout(function() {
+
+          }, 1000)
+        }
+        setTimeout(function(){
+            var rd = Math.random();
+            while(rd) {
+                if (rd > 0.1) {
+                    if (signal['id'] && signal['command'] === 'create') {
+                        var choose = $(':checked');
+                        var shipPowerTypeId = choose[0].value-1;
+                        var shipEnergyTypeId = choose[1].value-1;
+                        ship = new Ship(signal['id'], shipPowerTypeId, shipEnergyTypeId);
+                        console.log(ship);
+                        ship.recharge();
+                    } else {
+                        for (var i = 0; i < 4; i++) {
+                            if (shipSlot[i]) {
+                                shipSlot[i].signalRece(signalBi);
+                            }
+                            console.log('This message send!');
+                        }
+                    }
+                    break;
+                } else {
+                    console.log('This message lost! Try again!');
+                    rd = Math.random();
+                }
+            }
+        }, 300);
+        // console.log(shipSlot);
+    }
+
+    function bindDom(command) {
+        $('.' + command).each(function(index) {
+            $(this).click(function() {
+                BUS({
+                    'id': index + 1,
+                    'command': command
+                })
+            });
+        });
+    }
+    bindDom('create');
+    bindDom('launch');
+    bindDom('stop');
+    bindDom('destroy');
+});
